82339e4c9f24420bbdb26fd5b8a782cf1b78a0b5
ДМИТРИЙ-ПК\Дмитрий <zahod5277@mail.ru>
2017-05-05 22:35:46 +0300

    Задачи от Игоря май - генерация тайтлов

--- creonagency.ru/core/elements/snippets/getContentFirstP.php
+++ creonagency.ru/core/elements/snippets/getContentFirstP.php
@@ -8,6 +8,6 @@ if ($page = $modx->getObject('modResource',$id)){
     $content = explode('.', strip_tags($page->get('content')));
     return substr($content[0],0,260).'...';
 } else {
-    return $page->get('pagetitle');
+    return '';
 }
 
